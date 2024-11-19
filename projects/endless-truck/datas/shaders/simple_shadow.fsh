0687e98c14b691b990f99d5896422c6d272904e4
TonyMac129 <mac.web.company@gmail.com>
2024-11-18 19:57:46 -0500

    Maintenance over

--- projects/endless-truck/datas/shaders/simple_shadow.fsh
+++
@@ -1,7 +0,0 @@
-precision mediump float;
-varying vec4 MyColor;
-
-void main(void) 
-{
-        gl_FragColor = MyColor;
-}
--- projects/parking-fury-2/version.js
+++
@@ -1,2 +0,0 @@
-version="cc2";
-libs=["js/phaser/phaser-ads.min.js","js/phaser/phaser-cachebuster.min.js","js/phaser/phaser-super-storage.min.js","js/phaser/phaser-spine.min.js","js/phaser/splash.min.js"];
